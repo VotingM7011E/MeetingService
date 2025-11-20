3e593d4bf1bd8e5e67dcbe6c1993a0bfa4e3d002
Anton Nyström <antonn@ludd.ltu.se>
2025-11-20 15:57:14 +0100

    Add mongodb test

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
     return 'Hello from GHCR + Argo CD!'
 
+@app.route("/testdb")
+def test_db():
+    # Insert a test document
+    mongo.db.testcollection.insert_one({"status": "ok"})
+
+    # Fetch documents
+    docs = list(mongo.db.testcollection.find({}, {"_id": 0}))
+
+    return jsonify({"documents": docs})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
